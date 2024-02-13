fdb473e5d6741cf18c38263ffa801e144163633a
René Moser <mail@renemoser.net>
2024-02-13 17:50:37 +0100

    show retries in request error msg (#114)

--- plugins/module_utils/vultr_v2.py
+++ plugins/module_utils/vultr_v2.py
@@ -171,6 +171,11 @@ class AnsibleVultr:
             # Vultr has a rate limiting requests per second, try to be polite
             # Use exponential backoff plus a little bit of randomness
             backoff(retry=retry, retry_max_delay=retry_max_delay)
+        else:
+            self.module.fail_json(
+                msg='Failure while calling the Vultr API v2 with %s for "%s" with %s retries' % (method, path, retry + 1),
+                fetch_url_info=info,
+            )
 
         # Success with content
         if info["status"] in (200, 201, 202):
