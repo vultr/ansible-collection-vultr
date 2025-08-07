57a76d7bc145fee8deef398b5478e42dd88f4a39
Jeff Sites <jsites@vultr.com>
2025-08-07 11:13:20 -0400

    blankspace changes for sanity test passing

--- plugins/modules/load_balancer_info.py
+++ plugins/modules/load_balancer_info.py
@@ -15,123 +15,123 @@ module: load_balancer_info
 short_description: Get information about Vultr load balancers
 version_added: "1.13.0"
 description:
-	- Retrieve details about load balancers on Vultr.
+  - Retrieve details about load balancers on Vultr.
 author:
-	- "Garrett Haughawout (@ghaughawout)"
+  - "Garrett Haughawout (@ghaughawout)"
 extends_documentation_fragment:
-	- vultr.cloud.vultr_v2
+  - vultr.cloud.vultr_v2
 """
 
 EXAMPLES = """
 ---
 - name: Get Vultr load balancer information
-	vultr.cloud.load_balancer_info:
-	register: result
+  vultr.cloud.load_balancer_info:
+  register: result
 
 - name: Print the information
-	ansible.builtin.debug:
-	var: result.vultr_load_balancer_info
+  ansible.builtin.debug:
+  var: result.vultr_load_balancer_info
 """
 
 RETURN = """
 ---
 vultr_api:
-	description: Response from Vultr API with additional details.
-	returned: success
-	type: dict
-	contains:
-		id:
-			description: Unique ID of the load balancer.
-			returned: success
-			type: str
-			sample: "cb67a646-66fd-4dfb-b839-443f2e6c0b60"
-		date_created:
-			description: Date and time when the load balancer was created.
-			returned: success
-			type: str
-			sample: "2020-10-10T01:56:20+00:00"
-		region:
-			description: Region of the load balancer.
-			returned: success
-			type: str
-			sample: "ewr"
-		label:
-			description: Label of the load balancer.
-			returned: success
-			type: str
-			sample: "Example Load Balancer"
-		status:
-			description: Status of the load balancer.
-			returned: success
-			type: str
-			sample: "pending"
-		ipv4:
-			description: IPv4 address of the load balancer.
-			returned: success
-			type: str
-			sample: "192.0.2.1"
-		ipv6:
-			description: IPv6 address of the load balancer.
-			returned: success
-			type: str
-			sample: "2001:db8::1"
-		generic_info:
-			description: Generic information about the load balancer.
-			returned: success
-			type: dict
-		health_check:
-			description: Health check configuration for the load balancer.
-			returned: success
-			type: dict
-		has_ssl:
-			description: Whether SSL is enabled for the load balancer.
-			returned: success
-			type: bool
-			sample: false
-		http2:
-			description: Whether HTTP/2 is enabled.
-			returned: success
-			type: bool
-			sample: false
-		http3:
-			description: Whether HTTP/3 is enabled.
-			returned: success
-			type: bool
-			sample: false
-		nodes:
-			description: Number of nodes in the load balancer.
-			returned: success
-			type: int
-			sample: 1
-		forwarding_rules:
-			description: List of forwarding rules for the load balancer.
-			returned: success
-			type: list
-			elements: dict
-		firewall_rules:
-			description: Firewall rules applied to the load balancer.
-			returned: success
-			type: list
-			elements: dict
-		instances:
-			description: List of instances attached to the load balancer.
-			returned: success
-			type: list
-			elements: str
-		node_ips:
-			description: List of node IPs in the load balancer.
-			returned: success
-			type: list
-			elements: str
-		auto_ssl:
-			description: Auto SSL configuration for the load balancer.
-			returned: success
-			type: dict
-		global_regions:
-			description: Global regions configuration for the load balancer.
-			returned: success
-			type: list
-			elements: str
+  description: Response from Vultr API with additional details.
+  returned: success
+  type: dict
+  contains:
+    id:
+      description: Unique ID of the load balancer.
+      returned: success
+      type: str
+      sample: "cb67a646-66fd-4dfb-b839-443f2e6c0b60"
+    date_created:
+      description: Date and time when the load balancer was created.
+      returned: success
+      type: str
+      sample: "2020-10-10T01:56:20+00:00"
+    region:
+      description: Region of the load balancer.
+      returned: success
+      type: str
+      sample: "ewr"
+    label:
+      description: Label of the load balancer.
+      returned: success
+      type: str
+      sample: "Example Load Balancer"
+    status:
+      description: Status of the load balancer.
+      returned: success
+      type: str
+      sample: "pending"
+    ipv4:
+      description: IPv4 address of the load balancer.
+      returned: success
+      type: str
+      sample: "192.0.2.1"
+    ipv6:
+      description: IPv6 address of the load balancer.
+      returned: success
+      type: str
+      sample: "2001:db8::1"
+    generic_info:
+      description: Generic information about the load balancer.
+      returned: success
+      type: dict
+    health_check:
+      description: Health check configuration for the load balancer.
+      returned: success
+      type: dict
+    has_ssl:
+      description: Whether SSL is enabled for the load balancer.
+      returned: success
+      type: bool
+      sample: false
+    http2:
+      description: Whether HTTP/2 is enabled.
+      returned: success
+      type: bool
+      sample: false
+    http3:
+      description: Whether HTTP/3 is enabled.
+      returned: success
+      type: bool
+      sample: false
+    nodes:
+      description: Number of nodes in the load balancer.
+      returned: success
+      type: int
+      sample: 1
+    forwarding_rules:
+      description: List of forwarding rules for the load balancer.
+      returned: success
+      type: list
+      elements: dict
+    firewall_rules:
+      description: Firewall rules applied to the load balancer.
+      returned: success
+      type: list
+      elements: dict
+    instances:
+      description: List of instances attached to the load balancer.
+      returned: success
+      type: list
+      elements: str
+    node_ips:
+      description: List of node IPs in the load balancer.
+      returned: success
+      type: list
+      elements: str
+    auto_ssl:
+      description: Auto SSL configuration for the load balancer.
+      returned: success
+      type: dict
+    global_regions:
+      description: Global regions configuration for the load balancer.
+      returned: success
+      type: list
+      elements: str
 """
 
 from ansible.module_utils.basic import AnsibleModule
